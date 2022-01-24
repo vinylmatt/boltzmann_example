580d1962acd1d1c316a96b802daf659be04476b1
vinylmatt <matt@vinyl-matt.com>
2022-01-24 08:10:00 +0000

    working

--- money_model.py
+++ money_model.py
@@ -1,54 +1,32 @@
 from mesa import Agent, Model
 from mesa.time import RandomActivation
-# multiple agents in the same cell
-from mesa.space import MultiGrid
 
 class MoneyAgent(Agent):
     """ An agent with fixed initial wealth."""
     def __init__(self, unique_id, model):
         super().__init__(unique_id, model)
         self.wealth = 1
-        #debug
         print ("Hi, I am agent " + str(self.unique_id) +". My wealth is " + str(self.wealth) +".")       
-          
-    def move(self):
-        possible_steps = self.model.grid.get_neighborhood(
-            self.pos,
-            moore=True,
-            include_center=False)
-        new_position = self.random.choice(possible_steps)
-        self.model.grid.move_agent(self, new_position)
 
-    def give_money(self):
-        cellmates = self.model.grid.get_cell_list_contents([self.pos])
-        if len(cellmates) > 1:
-            other = self.random.choice(cellmates)
-            other.wealth += 1
-            self.wealth -= 1
-            #debug
-            print ("agent " + str(self.unique_id) +". gave to " + str(other.unique_id) +". wealth is " + str(self.wealth) +".")       
 
     def step(self):
-        self.move()
-        if self.wealth > 0:
-            self.give_money()
+        if self.wealth == 0:
+            return
+        other_agent = self.random.choice(self.model.schedule.agents)
+        other_agent.wealth += 1
+        self.wealth -= 1
+        print ("agent " + str(self.unique_id) +". gave to " + str(other_agent.unique_id) +". My wealth is " + str(self.wealth) +".")       
 
 class MoneyModel(Model):
     """A model with some number of agents."""
-    def __init__(self, N, width, height):
+    def __init__(self, N):
         self.num_agents = N
-        self.grid = MultiGrid(width, height, True)
         self.schedule = RandomActivation(self)
         # Create agents
         for i in range(self.num_agents):
             a = MoneyAgent(i, self)
             self.schedule.add(a)
-            # Add the agent to a random grid cell
-            x = self.random.randrange(self.grid.width)
-            y = self.random.randrange(self.grid.height)
-            self.grid.place_agent(a, (x, y))
 
-    
 
 
     def step(self):
