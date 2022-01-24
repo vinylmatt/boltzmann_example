aff3f9c358bed871d3f3577b76ac87f08c5c6016
vinylmatt <matt@vinyl-matt.com>
2022-01-24 08:19:47 +0000

    Get it working

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+from bleach import clean
 from money_model import MoneyModel
 import matplotlib.pyplot as plt
 import numpy as np
@@ -5,15 +6,20 @@ import numpy as np
 
 agent_wealth = []
 
-for j in range(100):
+for j in range(10):
     # Run the model
-    model = MoneyModel(10)
+    model = MoneyModel(100, 10, 10)
     for i in range(10):
         model.step()
 
     # Store the results
     for agent in model.schedule.agents:
         agent_wealth.append(agent.wealth)
-
+clean
 plt.hist(agent_wealth, bins=range(max(agent_wealth)+1))
 # %%
+# %%
+
+# %%
+
+
