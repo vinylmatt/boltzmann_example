fc0c9734b532ec3d4d6fb1247a31f90a6618e960
vinylmatt <matt@vinyl-matt.com>
2022-01-24 10:05:29 +0000

    working gini

--- money_model.py
+++ money_model.py
@@ -2,6 +2,14 @@ from mesa import Agent, Model
 from mesa.time import RandomActivation
 # multiple agents in the same cell
 from mesa.space import MultiGrid
+from mesa.datacollection import DataCollector
+
+def compute_gini(model):
+    agent_wealths = [agent.wealth for agent in model.schedule.agents]
+    x = sorted(agent_wealths)
+    N = model.num_agents
+    B = sum( xi * (N-i) for i,xi in enumerate(x) ) / (N*sum(x))
+    return (1 + (1/N) - 2*B)
 
 class MoneyAgent(Agent):
     """ An agent with fixed initial wealth."""
@@ -28,7 +36,7 @@ class MoneyAgent(Agent):
             other_agent.wealth += 1
             self.wealth -= 1
             #debug
-            #print ("agent " + str(self.unique_id) +". gave to " + str(other.unique_id) +". wealth is " + str(self.wealth) +".")       
+            print ("agent " + str(self.unique_id) +". gave to " + str(other_agent.unique_id) +". wealth is " + str(self.wealth) +".")       
 
     def step(self):
         self.move()
@@ -49,8 +57,14 @@ class MoneyModel(Model):
             x = self.random.randrange(self.grid.width)
             y = self.random.randrange(self.grid.height)
             self.grid.place_agent(a, (x, y))
+        
+        # Add datacollector
+        self.datacollector = DataCollector(
+            model_reporters={"Gini": compute_gini},
+            agent_reporters={"Wealth": "wealth"})
 
     def step(self):
+        self.datacollector.collect(self)
         self.schedule.step()
 
     
