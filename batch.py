02cf2a5df000f8cc71a808974b09d4127f6f9c43
vinylmatt <matt@vinyl-matt.com>
2022-01-24 20:44:19 +0000

    batch working

--- batch.py
+++ batch.py
@@ -1,7 +1,10 @@
 from bleach import clean
 from money_model import MoneyModel
 import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt1
 from mesa.batchrunner import BatchRunner
+import pandas as pd
+import numpy as np
 
 def compute_gini(model):
     agent_wealths = [agent.wealth for agent in model.schedule.agents]
@@ -13,17 +16,41 @@ def compute_gini(model):
 fixed_params = {"width": 10,
                "height": 10}
 variable_params = {"N": range(10, 500, 10)}
+#The total number of runs is 245. 
+# That is 10 agents to 490 increasing by 10, 
+# making 49 agents populations. 
+# Each agent population is then run 5 times 
+# (n of agent * 5) for 245 iterations
 
 batch_run = BatchRunner(MoneyModel,
                         variable_params,
                         fixed_params,
-                        iterations=5,
+                        # Each agent population is  run 5 times
+                        iterations=1,
+                        # Each agent population is  run 5 times
                         max_steps=100,
                         model_reporters={"Gini": compute_gini})
-batch_run.run_all()
 
+                        
+batch_run.run_all()
+# %%
 run_data = batch_run.get_model_vars_dataframe()
 run_data.head()
 # %%
 plt.scatter(run_data.N, run_data.Gini)
 # %%
+
+#Get the Agent DataCollection
+data_collector_agents = batch_run.get_collector_agents()
+values = data_collector_agents.values()
+print(values)
+
+
+#Get the Model DataCollection.
+
+data_collector_model = batch_run.get_collector_model()
+values = data_collector_model.values()
+print(values)
+# %%
+
+
