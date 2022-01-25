08922dd7080a54a1450aba971e9793c01ef2aea1
vinylmatt <matt@vinyl-matt.com>
2022-01-25 05:42:39 +0000

    Test readme layout with foobar example

--- batch.py
+++ batch.py
@@ -16,18 +16,26 @@ def compute_gini(model):
 fixed_params = {"width": 10,
                "height": 10}
 variable_params = {"N": range(10, 500, 10)}
+#variable_params = {"N": range(10, 500, 10)}
 #The total number of runs is 245. 
 # That is 10 agents to 490 increasing by 10, 
 # making 49 agents populations. 
 # Each agent population is then run 5 times 
 # (n of agent * 5) for 245 iterations
 
+#Faster
+#variable_params = {"N": range(10, 100, 10)}
+# The total number of runs is 45. 
+# That is 10 agents to 90 increasing by 10, 
+# making 9 agents populations. 
+# Each agent population is then run 5 times 
+# (n of agent * 5) for 45 iterations
+
 batch_run = BatchRunner(MoneyModel,
                         variable_params,
                         fixed_params,
                         # Each agent population is  run 5 times
-                        iterations=1,
-                        # Each agent population is  run 5 times
+                        iterations=5,
                         max_steps=100,
                         model_reporters={"Gini": compute_gini})
 
@@ -44,7 +52,6 @@ plt.scatter(run_data.N, run_data.Gini)
 data_collector_agents = batch_run.get_collector_agents()
 values = data_collector_agents.values()
 print(values)
-
 
 #Get the Model DataCollection.
 
